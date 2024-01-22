a681487edb40b2e34410718433b27ebf6b389f92
Judoloko <judoloko.85@gmail.com>
2024-01-22 19:59:12 -0300

    aula04

---
+++ Javascript-Basico/aula04/srcipt.js
@@ -0,0 +1,36 @@
+// comentario de uma linha 
+
+/*
+  comentario de multiplas linhas 
+*/ 
+
+/*
+Um programa de computador é um lista de "instruções" a serem "executados" por um computador.
+
+Um programa JavaScript é uma lista de instruções de programação.
+
+Valores, operadores, expressões, palavras-chave e comentários.
+
+Palavras chave 
+
+break
+continue 
+debugger
+do ... while
+for
+function
+if ...
+return
+switch 
+try ...
+var
+let
+const
+document
+
+*/
+
+console.log('hello world');
+console.log('hello world 2');
+
+document.querySelector('#elemento').src = 'imagem.jpg'
