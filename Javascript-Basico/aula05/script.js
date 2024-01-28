5a684da46a70d65be17ed66ca9ba1f8a55afa19a
Judoloko <judoloko.85@gmail.com>
2024-01-28 18:05:02 -0300

    aula05

---
+++ Javascript-Basico/aula05/script.js
@@ -0,0 +1,13 @@
+// var, let, const
+
+// var nome = 'Daniel'; // String
+// var idade = 24; // number
+
+// let nome = 'Daniel';
+
+
+const nome = "Daniel";
+const nomeCompletoPessoa = 'Daniel'; //camel case
+
+
+console.log(nome);
