4f5f9de0c64a07faef38da3da4892feb06113c50
Judoloko <judoloko.85@gmail.com>
2024-02-08 05:58:28 -0300

    aula09

---
+++ Javascript-Basico/aula09/script.js
@@ -0,0 +1,8 @@
+
+let nome = prompt("Digite seu nome");
+
+if (nome === "Daniel" || nome === "daniel" ) {
+  document.write(`Olá ${nome}, seja bem vindo!`)
+} else {
+  document.write("Você não tem acesso de administrador!")
+}
